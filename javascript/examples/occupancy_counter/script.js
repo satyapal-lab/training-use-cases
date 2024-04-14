938ac731cf5d93dbb3a58b83799b54ea5f5d3ae3
Satyapal Singh <satyapal.singh@nagarro.com>
2024-04-14 21:08:55 +0530

    javascript classes

--- javascript/examples/occupancy_counter/script.js
+++ javascript/examples/occupancy_counter/script.js
@@ -1,37 +1,106 @@
-const capacity = 20;
-let counter = 0;
+class Group {
+    #id;
+    #name;
+    #mobile;
+    #count;
 
-let inButton = document.getElementById("inButton");
-let outButton = document.getElementById("outButton");
-let counterSpan = document.getElementById("counter");
+    static counter = 0;
 
-counterSpan.style.color = "red";
-counterSpan.innerText = counter;
+    constructor(name, mobile, count) {
+        this.#name = name;
+        this.#mobile = mobile;
+        this.#count = count;
+        this.#id = ++Group.counter;
+    }
 
+    get name() {
+        return this.#name;
+    }
 
-function refreshCounter() {
-    counterSpan.innerText = counter;
+    get mobile() {
+        return this.#mobile;
+    }
+
+    get count() {
+        return this.#count;
+    }
+
+    get id() {
+        return this.#id;
+    }
+
 }
 
+class Club {
+    #capacity;
+    #counter;
+
+    #groups = [];
+
+    constructor(capacity) {
+        this.#capacity = capacity;
+        this.#counter = 0;
+        this.render();
+    }
+
+    in(group) {
+        const newCounter = this.#counter + group.count;
+
+        if (this.#capacity >= newCounter) {
+            this.#counter = newCounter;
+            this.#groups.push(group);
+            this.render();
+        }
+        else {
+            alert("Club is full.")
+        }
+    }
+
+    out(group) {
+        const index = this.#groups.findIndex((g) = g.id = group.id);
+        const removedGroups = this.#groups.splice(index, 1);
+        this.#counter -= removedGroups[0].count;
+        this.render();
+    }
 
 
+    render() {
+        let counterSpan = document.getElementById("counter");
+        counterSpan.style.color = "red";
+        counterSpan.innerText = this.#counter;
+
+        document.createElement("ul").appendChild()
+        this.#groups.forEach((group) => {
+           
+        })
+      
+    }
+}
+
+const club = new Club(50);
+
+const nameInput = document.getElementById("nameInput");
+const mobileInput = document.getElementById("mobileInput");
+const countInput = document.getElementById("countInput");
+
+let inButton = document.getElementById("inButton");
 inButton.addEventListener("click", () => {
-    if (counter < capacity) {
-        counter = counter + 1;
-        refreshCounter();
-    }
-    else {
-        alert("Club is full.")
-    }
-
+    const name = nameInput.value;
+    const mobile = mobileInput.value;
+    const count = parseInt(countInput.value);
+    const group = new Group(name, mobile, count);
+    club.in(group);
 });
 
-outButton.addEventListener("click", () => {
-    if (counter > 0) {
-        counter = counter - 1;
-        refreshCounter();
-    } else {
-        alert("club is empty");
-    }
+// let outButton = document.getElementById("outButton");
+// outButton.addEventListener("click", () => {
+//     if (counter > 0) {
+//         counter = counter - 1;
+//         refreshCounter();
+//     } else {
+//         alert("club is empty");
+//     }
 
-});
+// });
+
+
